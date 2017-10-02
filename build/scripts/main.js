bd0c77ee4464e14dc396f324f6ed9071389f068a
Brian Lam <iBriaan@gmail.com>
2017-10-01 21:21:56 -0700

    JS minified assets, build directory, load time and time

---
+++ build/scripts/main.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+    UpdateDateTime();
+});
+
+/*
+Load the date and populate the DOM.
+*/
+function LoadDate()
+{
+    // Date
+    var dateCurrentDate = new Date();
+    var dateMonth = getMonthString(dateCurrentDate.getMonth());
+    var dateDay = dateCurrentDate.getDate();
+    var dateString = dateMonth + " " + dateDay;
+    $(".greeting-date").text(dateString);
+    return;
+}
+
+/*
+Load the time and populate the DOM.
+*/
+function LoadTime()
+{
+    var timeCurrent = new Date();
+    var timeHours = timeCurrent.getHours();
+    var timeMinutes = timeCurrent.getMinutes();
+    var timeSeconds = timeCurrent.getSeconds();
+    var timeString = timeHours + ":" + TwoDigitString(timeMinutes) + ":" + TwoDigitString(timeSeconds);
+    $(".greeting-time").text(timeString);
+    
+    return;
+}
+
+/*
+Run LoadDate and LoadTime on an interval
+*/
+function UpdateDateTime()
+{
+    var intervalDate = setInterval(LoadDate, 1000);
+    var intervalTime = setInterval(LoadTime, 1000);
+}
+
+/*
+Show two digits for minutes and time. 
+
+Source: 
+*/
+function TwoDigitString(_input)
+{
+    return ('0' + _input).slice(-2);
+}
+
+/*
+Translate a month number into a string. 0-index based, 
+so Month 0 is January.
+
+Source: https://www.w3schools.com/jsref/jsref_getmonth.asp
+*/
+function getMonthString(month)
+{
+    if (month < 0 || month > 11)
+    {
+        return ""; 
+    }
+
+    var months = new Array();
+    months[0] = "January";
+    months[1] = "February";
+    months[2] = "March";
+    months[3] = "April";
+    months[4] = "May";
+    months[5] = "June";
+    months[6] = "July";
+    months[7] = "August";
+    months[8] = "September";
+    months[9] = "October";
+    months[10] = "November";
+    months[11] = "December";
+
+    return months[month];
+}
