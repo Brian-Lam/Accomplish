e7f4444f3cc35bbc0468257c8d3585f93698baf0
Brian Lam <iBriaan@gmail.com>
2017-10-03 22:15:53 -0700

    Update backgrounds

--- build/scripts/background.js
+++ build/scripts/background.js
@@ -6,17 +6,20 @@ function ShowBackground()
 {
     var backgrounds = 
     [
+        "above-adventure-aerial-air.jpg",
         "amazing-animal-beautiful-beautifull.jpg",
         "cairn-fog-mystical-background-158607.jpeg",
         "delicate-arch-night-stars-landscape.jpg",
         "pexels-photo-132037.jpeg",
         "pexels-photo-206359.jpeg",
+        "pexels-photo-247599.jpeg",
         "pexels-photo-262669.jpeg",
+        "pexels-photo-302740.jpeg",
         "pexels-photo-314860.jpeg",
         "pexels-photo-360912.jpeg",
+        "pexels-photo-375732.jpeg",
         "pexels-photo-552784.jpeg",
         "StockSnap_RGOP31CS3I.jpg",
-        "StockSnap_UZ06B1MM4D.jpg",
         "sunrise-phu-quoc-island-ocean.jpg"
     ];
 
